a383b08270e6c7eb8e8e8264aecc2898d952f27d
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-01-28 12:34:12 +0100

    solution

--- src/index.js
+++ src/index.js
@@ -1,46 +1,76 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+  /** make an array of morse letters */
+  let letters = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    let codedLetter = expr.substring(i, i + 10);
+    let decodedLetter = '';
+
+    for (let j = 0; j < codedLetter.length; j += 2) {
+      let symbol = codedLetter.substring(j, j + 2);
+      if (symbol == '10') {
+        decodedLetter += '.';
+      } else if (symbol == '11') {
+        decodedLetter += '-';
+      }
+    }
+    letters.push(decodedLetter);
+  }
+  // console.log(letters);
+
+  /** turn morse letters to latin letters */
+  let morseArray = letters.map(function (letter) {
+    return letter == '' ? ' ' : MORSE_TABLE[letter];
+  });
+  // console.log(morseArray)
+
+  /** return result */
+  return String(morseArray.join(''));
 }
 
+// decode(
+//   '00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010'
+// );
+// console.log('hello world');
+
 module.exports = {
-    decode
-}
+  decode,
+};
